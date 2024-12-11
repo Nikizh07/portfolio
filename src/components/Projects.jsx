aa1eaef0ef193c06f9e2cbaab52dc4647c2b1172
Nikizh <nikizh007@gmail.com>
2024-12-11 15:05:47 +0530

    blank pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Heading from './Heading'
+import DarkModeButton from './DarkModeButton'
+import NavContainer from './NavContainer'
+import MobileMenu from './MobileMenu'
 
 const Projects = () => {
   return (
     <div>
+      <div className="container lg:mx-[150px] p-4">
+        <div className="headrr flex">
+          <div className="my-[15px] lg:mr-[200px]">
+            <Heading />
+          </div>
+          <div className="dmode right-5 top-10 absolute">
+            <DarkModeButton />
+          </div>
 
-      
+          <div className="fuck my-[20px] hidden lg:flex">
+            <NavContainer />
+          </div>
+        </div>
+        <br />
+        <MobileMenu/>
+        <br />
+        <div className="hhding">
+          <p className="lnk-title">Projects</p>
+        </div>
+
+
+        </div>
     </div>
   )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Heading from './Heading'
+import DarkModeButton from './DarkModeButton'
+import NavContainer from './NavContainer'
+import MobileMenu from './MobileMenu'
 
 const Resume = () => {
   return (
     <div>
-      
+      <div className="container lg:mx-[150px] p-4">
+        <div className="headrr flex">
+          <div className="my-[15px] lg:mr-[200px]">
+            <Heading />
+          </div>
+          <div className="dmode right-5 top-10 absolute">
+            <DarkModeButton />
+          </div>
+
+          <div className="fuck my-[20px] hidden lg:flex">
+            <NavContainer />
+          </div>
+        </div>
+        <br />
+        <MobileMenu/>
+        <br />
+        <div className="hhding">
+          <p className="lnk-title">Resume</p>
+        </div>
+
+
+        </div>
     </div>
   )
 }
